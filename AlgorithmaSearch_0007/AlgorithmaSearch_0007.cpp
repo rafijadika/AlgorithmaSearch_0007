ac66fa3fbec8ab946ba689286b364438162d9522
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-04-05 15:19:37 +0700

    3

--- AlgorithmaSearch_0007/AlgorithmaSearch_0007.cpp
+++ AlgorithmaSearch_0007/AlgorithmaSearch_0007.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int arr[20]; // Array to be searched
 int n;       // Number of elements in the array
+
+void input() {
+    while (true) {
+        cout << "masukan jumlah element array: ";
+        cin >> n;
+        if (n > 0 && n <= 20) {
+            break;
+        }
+        else {
+            cout << "\narray harus mempunyai minim 1 dan maksimum 20.\n\n";
+        }
+    }
